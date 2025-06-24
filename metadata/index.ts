1633338049f2a6cf93c544314449c278deb64b14
berzan <berzan@monda.exchange>
2025-06-24 15:46:55 +0300

    Implement metadata generation

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello via Bun!");
+function createMetadataObject(props: {
+  number: number
+  name: string
+  description: string
+  image: `ipfs://${string}`
+  attributes: Array<{
+    trait_type: string
+    value: string
+  }>
+}) {
+  return {
+    name: `${props.name} #${props.number}`,
+    description: `${props.description}`,
+    image: `${props.image}`,
+    attributes: props.attributes,
+  } as const
+}
+
+for (let i = 0; i < 33; i++) {
+  const metadataObject = createMetadataObject({
+    number: i,
+    name: 'Keonehorse',
+    description: 'Keonehorse is the smartest and humblest human horse.',
+    image: 'ipfs://CID/keonehorse.jpeg',
+    attributes: [
+      { trait_type: 'Kind', value: 'Human horse' },
+      { trait_type: 'Intelligence', value: 'Wonderful' },
+      { trait_type: 'Humbleness', value: 'Amazing' },
+    ],
+  })
+
+  await Bun.write(`out/${i}`, JSON.stringify(metadataObject), { createPath: true })
+}
