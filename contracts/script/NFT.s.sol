22ddc1142077824e9ef18316c1be5614d3acdd8d
berzan <berzan@monda.exchange>
2025-06-24 17:42:11 +0300

    Update script

--- contracts/script/NFT.s.sol
+++ contracts/script/NFT.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 import {NFT} from "../src/NFT.sol";
 
-contract CounterScript is Script {
+contract NftScript is Script {
     NFT public nft;
 
     function setUp() public {}
