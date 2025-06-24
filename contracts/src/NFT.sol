3e31dbb954d6ddfca0a51a61bbc15d5ea9364072
berzan <berzan@monda.exchange>
2025-06-24 15:50:15 +0300

    Implement NFT contract

--- contracts/src/NFT.sol
+++ contracts/src/NFT.sol
@@ -1,14 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity ^0.8.13;
 
-contract NFT {
-    uint256 public number;
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC2981} from "@openzeppelin/contracts/token/common/ERC2981.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {IERC2981} from "@openzeppelin/contracts/interfaces/IERC2981.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract NFT is ERC721Enumerable, ERC2981, Ownable {
+    constructor() ERC721("Keonehorse", "KEONEHORSE") Ownable(msg.sender) {
+        _setDefaultRoyalty(msg.sender, 500);
     }
 
-    function increment() public {
-        number++;
+    function mint() external payable {
+        uint256 tokenId = totalSupply();
+        require(tokenId < 33, "All supply is minted.");
+        require(msg.value == 1 ether, "1 MON is not paid.");
+        _safeMint(msg.sender, tokenId);
+        (bool ok,) = payable(owner()).call{value: 1 ether}("");
+        require(ok, "1 MON is not sent.");
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://CID/";
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721Enumerable, ERC2981) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
